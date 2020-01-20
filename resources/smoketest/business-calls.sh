1b6081cb9ec5ebaccf2f62dbc6c5dbda8e7f30bf
sfrevel <3075075+sfrevel@users.noreply.github.com>
2020-01-20 13:43:11 +0100

    introduce wait sequence for mockserver startup

--- resources/smoketest/business-calls.sh
+++ resources/smoketest/business-calls.sh
@@ -2,7 +2,9 @@
 
 java -Dmockserver.initializationJsonPath=./data/init_expectations.json -jar ./lib/mockserver-netty-5.8.1-jar-with-dependencies.jar -serverPort 9090 -logLevel ERROR &
 #wait while mockserver is staring
-sleep 3
+until $(curl --output /dev/null --silent --head --fail -X PUT "http://localhost:9090/status"); do
+    sleep 1
+done
 
 # purchase components
 curl -u admin:adminpwd --header "Content-Type: application/json" --request POST --data @data/component_demands.json http://localhost:9080/supplierdomain/supplier/purchase
@@ -11,4 +13,7 @@ curl -u admin:adminpwd --header "Content-Type: application/json" --request POST 
 # deliver components
 curl -u admin:adminpwd --header "Content-Type: application/json" --request POST http://localhost:9080/supplierdomain/supplier/process_delivery/1
 
+# wait for still executing calls to mockserver
+sleep 1
+# stop the mockserver
 curl -X PUT "http://localhost:9090/stop" -H  "accept: */*"
