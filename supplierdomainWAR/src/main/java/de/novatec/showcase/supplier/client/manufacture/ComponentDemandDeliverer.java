062446d15d00b34312c9fcdb80488d20864db90b
sfrevel <3075075+sfrevel@users.noreply.github.com>
2020-01-22 10:25:09 +0100

    [refactoring] add JNDI properties for manufacture domain

--- supplierdomainWAR/src/main/java/de/novatec/showcase/supplier/client/manufacture/ComponentDemandDeliverer.java
+++ supplierdomainWAR/src/main/java/de/novatec/showcase/supplier/client/manufacture/ComponentDemandDeliverer.java
@@ -2,6 +2,8 @@ package de.novatec.showcase.supplier.client.manufacture;
 
 import java.util.List;
 
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
@@ -11,6 +13,8 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.glassfish.jersey.client.authentication.HttpAuthenticationFeature;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
 
@@ -19,6 +23,10 @@ import de.novatec.showcase.manufacture.dto.ComponentDemands;
 
 public class ComponentDemandDeliverer {
 
+	private static final String JNDI_PROPERTY_MANUFACTUREDOMAIN_DELIVER_URL = "manufacturedomain.deliver.url";
+	private static final String JNDI_PROPERTY_MANUFACTUREDOMAIN_USERNAME = "manufacturedomain.username";
+	private static final String JNDI_PROPERTY_MANUFACTUREDOMAIN_PASSWORD = "manufacturedomain.password";
+	private static final Logger log = LoggerFactory.getLogger(ComponentDemandDeliverer.class);
 	private static final String USERNAME = System.getProperty("username.manufacture");
 	private static final String PASSWORD = System.getProperty("password.manufacture");
 	private static final String PORT = System.getProperty("http.port.manufacture");
@@ -26,6 +34,9 @@ public class ComponentDemandDeliverer {
 
 	private static final String COMPONENT_URL = BASE_URL + "component/";
 	private static final String DELIVER_URL = COMPONENT_URL + "deliver/";
+	private String deliverUrl = DELIVER_URL;
+	private String username = USERNAME;
+	private String password = PASSWORD;
 	private Client client;
 
 	public ComponentDemandDeliverer() {
@@ -33,10 +44,20 @@ public class ComponentDemandDeliverer {
 		client.register(JacksonJsonProvider.class);
 		HttpAuthenticationFeature feature = HttpAuthenticationFeature.basicBuilder().build();
 		client.register(feature);
-	}
+
+		try {
+			deliverUrl = (String)new InitialContext().lookup(JNDI_PROPERTY_MANUFACTUREDOMAIN_DELIVER_URL);
+			username = (String)new InitialContext().lookup(JNDI_PROPERTY_MANUFACTUREDOMAIN_USERNAME);
+			password = (String)new InitialContext().lookup(JNDI_PROPERTY_MANUFACTUREDOMAIN_PASSWORD);
+		} catch (NamingException e) {
+			log.warn("JNDI properties " + JNDI_PROPERTY_MANUFACTUREDOMAIN_DELIVER_URL + " or " +
+					JNDI_PROPERTY_MANUFACTUREDOMAIN_USERNAME + " or " +
+					JNDI_PROPERTY_MANUFACTUREDOMAIN_PASSWORD + " not found! Using system properties where possible!", e);
+		}
+}
 
 	public void deliver(List<ComponentDemand> componentDemands) throws RestcallException {
-		WebTarget target = client.target(DELIVER_URL);
+		WebTarget target = client.target(deliverUrl);
 		Response response = asAdmin(target.request(MediaType.APPLICATION_JSON_TYPE))
 				.post(Entity.json(new ComponentDemands().setComponentDemands(componentDemands)));
 		if (response.getStatus() == Response.Status.OK.getStatusCode()) {
@@ -46,8 +67,8 @@ public class ComponentDemandDeliverer {
 				"Http Status " + Response.Status.fromStatusCode(response.getStatus()) + " while calling " + DELIVER_URL + " with " + componentDemands + ". " + response.readEntity(String.class));
 	}
 
-	private static Builder asAdmin(Builder builder) {
-		return asUser(builder, USERNAME, PASSWORD);
+	private Builder asAdmin(Builder builder) {
+		return asUser(builder, username, password);
 	}
 
 	private static Builder asUser(Builder builder, String userName, String password) {
